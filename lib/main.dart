e64d3948f6dbbb9dee2ea8a4ca5ec6972f6a1574
ssdd33 <beeblu.0@gmail.com>
2023-02-28 19:16:42 +0900

    injecting providers

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_provider/pages/todos_page.dart';
+import 'package:flutter_provider/providers/providers.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,12 +12,35 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'TODOS',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<TodoFilter>(create: (context) => TodoFilter()),
+        ChangeNotifierProvider<TodoSearch>(create: (context) => TodoSearch()),
+        ChangeNotifierProvider<TodoList>(create: (context) => TodoList()),
+        ChangeNotifierProxyProvider<TodoList, ActiveTodoCount>(
+          create: (context) => ActiveTodoCount(),
+          update: (BuildContext context, TodoList todoList,
+                  ActiveTodoCount? activeTodoCount) =>
+              activeTodoCount!..update(todoList),
+        ),
+        ChangeNotifierProxyProvider3<TodoList, TodoFilter, TodoSearch,
+            FilteredTodos>(
+          create: (context) => FilteredTodos(),
+          update: (BuildContext context,
+                  TodoList todoList,
+                  TodoFilter todoFilter,
+                  TodoSearch todoSearch,
+                  FilteredTodos? filteredTodos) =>
+              filteredTodos!..update(todoFilter, todoList, todoSearch),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'TODOS',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: TodosPage(),
       ),
-      home: TodosPage(),
     );
   }
 }
