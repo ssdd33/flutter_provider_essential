982a2048bac18776fb3e7abce00f7ab667564e4e
ssdd33 <beeblu.0@gmail.com>
2023-02-28 19:05:08 +0900

    activeTodoCountState

--- lib/providers/active_todo_count.dart
+++ lib/providers/active_todo_count.dart
@@ -1,22 +1,45 @@
 import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_provider/models/todo_model.dart';
+import 'package:flutter_provider/providers/todo_list.dart';
 
 class ActiveTodoCountState extends Equatable {
-  final int count;
+  final int activeTodoCount;
   ActiveTodoCountState({
-    required this.count,
+    this.activeTodoCount = 0,
   });
 
-  @override
-  List<Object> get props => [count];
+  factory ActiveTodoCountState.initial() {
+    return ActiveTodoCountState(activeTodoCount: 0);
+  }
 
   @override
-  String toString() => 'ActiveTodoCountState(count: $count)';
+  List<Object> get props => [activeTodoCount];
+
+  @override
+  String toString() =>
+      'ActiveTodoCountState(activeTodoCount: $activeTodoCount)';
 
   ActiveTodoCountState copyWith({
-    int? count,
+    int? activeTodoCount,
   }) {
     return ActiveTodoCountState(
-      count: count ?? this.count,
+      activeTodoCount: activeTodoCount ?? this.activeTodoCount,
     );
+  }
+}
+
+class ActiveTodoCount with ChangeNotifier {
+  ActiveTodoCountState _state = ActiveTodoCountState.initial();
+  ActiveTodoCountState get state => _state;
+
+  void update(TodoList todoList) {
+    final int newCount = todoList.state.todos
+        .where((Todo todo) => !todo.completed)
+        .toList()
+        .length;
+
+    _state = _state.copyWith(activeTodoCount: newCount);
+    notifyListeners();
   }
 }
